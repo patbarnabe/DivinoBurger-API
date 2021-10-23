b6c9f8c76c4b0e6b237f781fa76e067fdf6c5064
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-10-23 13:48:59 -0300

    Installing morgan and handling errors on routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-console */
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const routes = require('./server/routes/index');
 
 const app = express();
@@ -10,10 +12,26 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(morgan('dev'));
 app.use('/', routes);
 
-app.get('*', (req, res) => {
-  res.send('Hello World!');
+app.get('/', (req, res) => {
+  res.send('Divino Burger API');
+});
+
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 app.listen(port, () => {
