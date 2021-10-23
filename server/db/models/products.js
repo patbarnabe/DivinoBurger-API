0d3946b79b289a9f33cb5a26fb6b7252d143388f
Patrícia Barnabé <patbarnabe5@gmail.com>
2021-10-23 02:12:41 -0300

    Cleaning code

--- server/db/models/products.js
+++ server/db/models/products.js
@@ -9,11 +9,6 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class Products extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       Products.belongsToMany(models.ProductsOrders,
         {
--- server/db/models/productsorders.js
+++ server/db/models/productsorders.js
@@ -9,11 +9,6 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class ProductsOrders extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       ProductsOrders.belongsTo(models.Orders, { foreignKey: 'order_id' });
       ProductsOrders.belongsTo(models.Products, { foreignKey: 'product_id' });
